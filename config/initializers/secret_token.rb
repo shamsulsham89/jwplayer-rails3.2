8f1eeb1a8e16d9b0f75a32ab70647a036c287e30
shamsul <shamsul@shamsul-VPCEH28FN.(none)>
2014-04-21 19:26:39 +0530

    jwplayer in rails 3.2

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Jwplayer::Application.config.secret_token = '1e6dc72d097b566bedf38e0a19a185bbf46f71088a9545c6720db80782f57c76115dca16ccd005435243e51a0933dea1ded90ac0febf7d45adf8993a83fe2e15'
